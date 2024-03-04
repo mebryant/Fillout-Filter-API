4cd38c74a8badcbee8b6b3724107026ae8ac0d69
michaelbryant <mbryant36@gmail.com>
2024-03-04 17:41:02 -0500

    More clear logging

--- app/services/filter.js
+++ app/services/filter.js
@@ -1,19 +1,18 @@
 const axios = require('axios').default;
 
-const axiosConfig = {
-    headers: { Authorization: `Bearer ${process.env.FILLOUT_API_KEY}`}
-}
-
 async function getByFormId(req, res) {
     const { formId } = req.params;
 
-    await axios.get(`${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId}`, axiosConfig)
+    console.log(`GET formData :: ${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId} w/ ${process.env.FILLOUT_API_KEY}`)
+    await axios.get(`${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId}`, {
+        headers: { Authorization: `Bearer ${process.env.FILLOUT_API_KEY}`}
+    })
         .then((data) => {
             res.send(data.data);
         })
         .catch((e) => {
-            console.log(e);
-            res.status(400).send(e.message);
+            console.log(e.response.data);
+            res.status(e.response.data.statusCode).send(e.response.data.message);
         })
 }
 
