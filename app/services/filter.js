225876691f256c3ab03a7893c4a72f8ce3f84b13
michaelbryant <mbryant36@gmail.com>
2024-03-04 17:45:25 -0500

    think render was deployments behind. Removing log for env

--- app/services/filter.js
+++ app/services/filter.js
@@ -3,7 +3,6 @@ const axios = require('axios').default;
 async function getByFormId(req, res) {
     const { formId } = req.params;
 
-    console.log(`GET formData :: ${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId} w/ ${process.env.FILLOUT_API_KEY}`)
     await axios.get(`${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId}`, {
         headers: { Authorization: `Bearer ${process.env.FILLOUT_API_KEY}`}
     })
