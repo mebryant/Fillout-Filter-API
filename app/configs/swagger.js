82f15e1daa9812ad28fb3bdbb817e36c8e87d4c8
michaelbryant <mbryant36@gmail.com>
2024-03-04 19:48:55 -0500

    Adding filter logic w/ swagger examples

--- app/configs/swagger.js
+++ app/configs/swagger.js
@@ -34,14 +34,107 @@ const options = {
                     summary: "Returns a filtered responses for given form ID",
                     description: "Returns default value for now",
                     parameters: [{
-                        name: "formId",
-                        in: "path",
-                        description: "Form ID",
-                        required: true,
+                          name: "formId",
+                          in: "path",
+                          description: "Form ID",
+                          required: true,
+                          schema: {
+                              type: "string",
+                              example: "cLZojxk94ous"
+                          }
+                      },
+                      {
+                        name: "limit",
+                        in: "query",
+                        description: "Limit. Number between 1 and 150. Default is 150.",
+                        required: false,
+                        schema: {
+                            type: "integer"
+                        }
+                      },
+                      {
+                        name: "afterDate",
+                        in: "query",
+                        description: "Date string to filter responses submitted after this date. Fmt: YYYY-MM-DDTHH:mm:ss.sssZ",
+                        required: false,
+                        schema: {
+                            type: "date"
+                        }
+                      },
+                      {
+                        name: "beforeDate",
+                        in: "query",
+                        description: "Date string to filter responses submitted before this date. Fmt: YYYY-MM-DDTHH:mm:ss.sssZ",
+                        required: false,
+                        schema: {
+                            type: "date"
+                        }
+                      },
+                      {
+                        name: "offset",
+                        in: "query",
+                        description: "Starting position from which to fetch the responses. Default is 0.",
+                        required: false,
+                        schema: {
+                            type: "integer"
+                        }
+                      },
+                      {
+                        name: "status",
+                        in: "query",
+                        description: "Pass in_progress to get a list of in-progress (unfinished) submissions. By default, only finished submissions are returned.",
+                        required: false,
+                        schema: {
+                            type: "string",
+                            enum: ["in_progress"]
+                        }
+                      },
+                      {
+                        name: "includeEditLink",
+                        in: "query",
+                        description: "Pass true to include a link to edit the submission as editLink",
+                        required: false,
                         schema: {
                             type: "string"
                         }
-                    }],
+                      },
+                      {
+                        name: "sort",
+                        in: "query",
+                        description: "Can be asc or desc, defaults to asc.",
+                        required: false,
+                        schema: {
+                            type: "string",
+                            enum: ["asc", "desc"]
+                        }
+                      },
+                      {
+                        name: "filters",
+                        in: "query",
+                        description: "Each of these filters should be applied like an AND in a 'where' clause",
+                        required: false,
+                        schema: {
+                            type: "array",
+                            items: {
+                              type: "object",
+                              properties: {
+                                id: {
+                                  type: "string",
+                                  example: "bE2Bo4cGUv49cjnqZ4UnkW"
+                                },
+                                condition: {
+                                  type: "string",
+                                  enum: ["equals", "does_not_equal", "greater_than", "less_than"]
+                                },
+                                value: {
+                                  type: "string",
+                                  example: "Johnny"
+                                }
+                              }
+                            }
+                        }
+                      } 
+                    ],
                     responses: {
                         '200' : {
                             description: "Successful",
@@ -52,7 +145,17 @@ const options = {
                                     }
                                }
                             }
-                        }
+                        },
+                        '400' : {
+                          description: "Bad Request",
+                          content: {
+                             "application/json": {
+                                  schema: {
+                                      type: "string"
+                                  }
+                             }
+                          }
+                      }
                     }
                 }
             }
--- app/services/filter.js
+++ app/services/filter.js
@@ -2,17 +2,81 @@ const axios = require('axios').default;
 
 async function getByFormId(req, res) {
     const { formId } = req.params;
+    const queryParams = req.query;
+    const filters = filtersToObject(queryParams.filters)
 
-    await axios.get(`${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId}`, {
+    if(!formId) {
+        res.status(400).send('"formId" is required!')
+    }
+
+    console.log(`GET :: ${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId}/submissions?${formatQueryParams(queryParams)}`)
+    await axios.get(`${process.env.FILLOUT_BASE_URL}/v1/api/forms/${formId}/submissions?${formatQueryParams(queryParams)}`, {
         headers: { Authorization: `Bearer ${process.env.FILLOUT_API_KEY}`}
     })
         .then((data) => {
-            res.send(data.data);
+            const originalSize = data.data.totalResponses;
+            const limit = queryParams.limit ? queryParams.limit : 150;
+            const responses = data.data.responses.filter((response) => filterResponseByQuestions(response.questions, filters));
+
+            res.send({
+                responses,
+                totalResponses: responses.length,
+                pageCount: Math.ceil(originalSize/limit)
+            });
         })
         .catch((e) => {
-            console.log(e.response.data);
-            res.status(e.response.data.statusCode).send(e.response.data.message);
+            console.log(e);
+            if(e.response.data){
+                res.status(e.response.data.statusCode).send(e.response.data.message);
+            } else {
+                res.status(400).send(e);
+            }
         })
+}
+
+function formatQueryParams(params){
+    let queryString = "";
+
+    Object.keys(params).forEach((key) => {
+        if(params[key] && key != 'filters') {
+            queryString += `${key}=${params[key]}`
+        }
+    })
+
+    return queryString
+}
+
+function parseCondition(condition, ob1, ob2) {
+    switch(condition) {
+        case "equals": 
+            return ob1 == ob2;
+        case "does_not_equal":
+            return ob1 != ob2;
+        case "greater_than":
+            return ob1 > ob2;
+        case "less_than":
+            return ob1 < ob2;
+        default:
+            throw new Error("Condition invalid!")
+
+    }
+}
+
+function filterResponseByQuestions(questions, filters){
+    const results = filters.map( filter => {
+        const question = questions.find( question => question.id == filter.id);
+        return parseCondition(filter.condition, question.value, filter.value)
+    })
+
+    return !results.includes(false)
+}
+
+function filtersToObject(filter){
+    if(Array.isArray(filter)){
+        return filter.map(f => JSON.parse(f))
+    } else {
+        return [JSON.parse(filter)]
+    }
 }
 
 module.exports = {
